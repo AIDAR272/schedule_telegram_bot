380b64741f17762e2d875fd460d748323434aba4
Aidar <aidar.nasirov_2028@ucentralasia.org>
2025-09-03 14:34:39 +0600

    add[get username]

--- db.py
+++ db.py
@@ -12,8 +12,9 @@ async def init_db(application):
     global db_pool
     db_pool = await asyncpg.create_pool(DATABASE_URL, min_size=1, max_size=5)
     async with db_pool.acquire() as conn:
+        await conn.execute("DROP TABLE IF EXISTS users")
         await conn.execute("""
-            CREATE TABLE IF NOT EXISTS Users (
+            CREATE TABLE IF NOT EXISTS users (
                 user_id BIGINT PRIMARY KEY,
                 first_name TEXT NOT NULL,
                 username TEXT
