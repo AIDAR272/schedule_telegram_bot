fd0b089b36c04ddcb54055671554457c94dd491c
Aidar <aidar.nasirov_2028@ucentralasia.org>
2025-09-03 16:10:38 +0600

    fix[db preparation]

--- db.py
+++ db.py
@@ -12,7 +12,7 @@ async def init_db(application):
     global db_pool
     db_pool = await asyncpg.create_pool(DATABASE_URL, min_size=1, max_size=5)
     async with db_pool.acquire() as conn:
-        await conn.execute("DROP TABLE IF EXISTS users")
+        #await conn.execute("DROP TABLE IF EXISTS users")
         await conn.execute("""
             CREATE TABLE IF NOT EXISTS users (
                 user_id BIGINT PRIMARY KEY,
