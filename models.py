65dc73fc77e10beaa91c520d609f3da8099ce610
Aidar <nasirovaidar11@gmail.com>
2025-11-18 00:26:57 +0600

    add[sqlachemy]

---
+++ models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, BigInteger
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(BigInteger, nullable=False)
+    first_name = Column(String(50))
+    username = Column(String(50))
+
+    events = relationship("Event", back_populates="user")
+
+
+class Event(Base):
+    __tablename__ = "events"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, nullable=False)
+    day = Column(String(50))
+    time = Column(String(50))
+    event = Column(String(50))
+
+    user = relationship("User", back_populates="events")
